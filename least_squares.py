0cf3634bd1d3bf0865ede13028fa0503f2ff6242
keko24 <kotevski_andrej@hotmail.com>
2024-09-17 01:59:36 +0200

    Implemented least_squares and ridge regression, and also added a function for plotting 1d regression data.

--- least_squares.py
+++ least_squares.py
@@ -1,13 +1,33 @@
-import matplotlib.pyplot as plt
 import numpy as np
+
+from utils import plot_regression_1d
 
 
 def least_squares(y, tx):
-    raise NotImplementedError
+    tx_t = np.transpose(tx)
+    return np.linalg.solve(tx_t @ tx, tx_t @ y)
 
 
 def main():
-    raise NotImplementedError
+    n, d = 100, 1
+
+    original_tx = np.random.randn(n, d)
+
+    bias = np.ones((n, 1))
+    tx = np.hstack((bias, original_tx))
+
+    w_true = np.random.randn(d + 1)
+
+    noise = np.random.randn(n)
+
+    y = tx @ w_true + noise
+
+    w_learned = least_squares(y, tx)
+
+    print("Learned weights:", w_learned)
+    print("True weights:", w_true)
+
+    plot_regression_1d(tx, y, w_learned)
 
 
 if __name__ == "__main__":
--- ridge_regression.py
+++ ridge_regression.py
@@ -1,13 +1,38 @@
-import matplotlib.pyplot as plt
 import numpy as np
+
+from utils import plot_regression_1d
 
 
 def ridge_regression(y, tx, lambda_):
-    raise NotImplementedError
+    tx_t = tx.T
+    identity = np.eye(tx.shape[1])
+
+    identity[0, 0] = 0
+    w = np.linalg.inv(tx_t @ tx + lambda_ * identity) @ tx_t @ y
+    return w
 
 
 def main():
-    raise NotImplementedError
+    n, d = 100, 1
+    lambda_ = 1
+
+    original_tx = np.random.randn(n, d)
+
+    bias = np.ones((n, 1))
+    tx = np.hstack((bias, original_tx))
+
+    w_true = np.random.randn(d + 1)
+
+    noise = np.random.randn(n)
+
+    y = tx @ w_true + noise
+
+    w_learned = ridge_regression(y, tx, lambda_)
+
+    print("Learned weights (including bias):", w_learned)
+    print("True weights (including bias):", w_true)
+
+    plot_regression_1d(tx, y, w_learned)
 
 
 if __name__ == "__main__":
