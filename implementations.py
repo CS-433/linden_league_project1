7ceda4f9d295f0b1fc28fa5fb6bf1488ed3bd5ca
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-09-24 17:33:06 +0200

    finish refactor

--- implementations.py
+++ implementations.py
@@ -16,6 +16,7 @@ def loss_mse(y, tx, w):
     """
     return np.square(y - tx @ w).mean()
 
+
 def compute_gradient_linreg(y, tx, w):
     """
     Calculate the gradient of the square mean error loss function of a linear regression model, with respect to the weights w.
@@ -28,7 +29,7 @@ def compute_gradient_linreg(y, tx, w):
     Returns:
         g: np.ndarray(D) : gradient of the loss with respect to w
     """
-    return -1/len(y) * np.transpose(tx) @ (y - tx @ w) 
+    return -1 / len(y) * np.transpose(tx) @ (y - tx @ w)
 
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
@@ -57,14 +58,12 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     for n_iter, batch in enumerate(batches):
         y_batch, xt_batch = batch
         g = compute_gradient_linreg(y_batch, xt_batch, w)
-        
+
         w = w - gamma * g
         ws.append(w)
 
         loss = loss_mse(y, tx @ w)
         losses.append(loss)
-
-
 
         print(
             "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
@@ -72,6 +71,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
             )
         )
     return ws[-1], losses[-1]
+
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """
@@ -108,6 +108,7 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
     return ws[-1], losses[-1]
 
+
 def least_squares(y, tx):
     """
     Computes the least squares solution to the linear regression problem.
@@ -124,11 +125,12 @@ def least_squares(y, tx):
     w = np.linalg.solve(tx_t @ tx, tx_t @ y)
 
     # Compute Mean Squared Error for the Loss
-    loss = 1/2 * np.square(y - tx @ w).mean()
+    loss = 1 / 2 * np.square(y - tx @ w).mean()
     return w, loss
 
+
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
-    """ Regularized logistic regression using gradient descent
+    """Regularized logistic regression using gradient descent
 
     Parameters:
         y : np.ndarray(N) : labels (0 or 1)
@@ -147,7 +149,7 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         ### compute grad and update w
         grad = log_reg_grad(y, tx, w) + 2 * lambda_ * w
         w -= gamma * grad
-    final_loss = log_reg_loss(y, tx, w) # don't include the regularization term
+    final_loss = log_reg_loss(y, tx, w)  # don't include the regularization term
 
     return w, final_loss
 
