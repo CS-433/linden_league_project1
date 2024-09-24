420b12b7f31c97302f4e3366fd5bdd7f799dda52
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-09-24 12:12:51 +0200

    add readme and run

--- implementatoins.py
+++ implementatoins.py
@@ -6,22 +6,6 @@ def compute_gradient(y, tx, w):
 
 
 def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
-    """The Stochastic Gradient Descent algorithm (SGD).
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
-        max_iters: a scalar denoting the total number of iterations of SGD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
-    """
-
-    # Define parameters to store w and loss
     ws = [initial_w]
     losses = []
     w = initial_w
@@ -29,12 +13,8 @@ def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
     batches = batch_iter(y, tx, batch_size, max_iters)
 
     for n_iter, batch in enumerate(batches):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: implement stochastic gradient descent.
-        # ***************************************************
         y_batch, xt_batch = batch
-        g = compute_stoch_gradient(y_batch, xt_batch, w)
+        g = compute_gradient(y_batch, xt_batch, w)
         loss = compute_loss(y, tx, w)
         
         w = w - gamma * g
@@ -51,9 +31,6 @@ def mean_squared_error_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
     return losses, ws
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
-    """The Gradient Descent (GD) algorithm.
-    """
-    # Define parameters to store w and loss
     ws = [initial_w]
     losses = []
     w = initial_w
